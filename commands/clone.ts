0a131c7f21c2bcf52e94841572399ff021d2f115
simple-me <you@example.com>
2023-12-04 20:57:05 -0300

    push

--- commands/clone.ts
+++ commands/clone.ts
@@ -6,10 +6,20 @@ import http from "isomorphic-git/http/node";
 import { RepoInfo } from "../interfaces/interfaces";
 
 export function cloneIndividualRepo(repoInfo: RepoInfo) {
-    const dir = path.join(process.cwd(), 'test-clone')
+    const fileName = repoInfo.url.split('/').pop();
+    if (fileName === undefined) {
+        throw new Error("error!")
+    }
+
+    const dir = path.join(process.cwd(), fileName);
+    console.log(dir);
+    
+    //const dir = path.join(process.cwd(), 'test-clone')
+    //const dir = path.join(process.cwd(), repoInfo.url.split('/').pop());
     git.clone({ fs, http, dir, url: repoInfo.url })
     .then((res) => {
-        console.log(res);
+        console.log(`repo cloned: ${fileName}`);
+        return dir;
     })
     .catch((e) => {
       console.log(e);
